15f60c7ca62c6df6d1bc4a7152034c65ebfee754
Hbooth078 <hbooth078@mustangs.deltacollege.edu>
2025-12-04 13:41:21 -0800

    Create lht_list.js

---
+++ lht_list.js
@@ -0,0 +1,45 @@
+/* Author: Hailee | Date: Dec 4, 2025
+   Event data arrays for Lyman Hall Theater
+*/
+
+let eventDates = [
+  "October 1, 2021 19:30",
+  "October 2, 2021 19:00",
+  "October 3, 2021 11:00",
+  "October 5, 2021 19:00",
+  "October 6, 2021 19:00",
+  "October 7, 2021 19:00",
+  "October 9, 2021 19:00",
+  "October 10, 2021 11:00",
+  "October 11, 2021 19:00",
+  "October 13, 2021 20:00",
+  "October 14, 2021 20:00"
+];
+
+let eventDescriptions = [
+  "Have Spacesuit, Will Travel",
+  "Cabaret",
+  "Classics Brunch",
+  "Visions of Light and Dreams",
+  "San Diego Blues",
+  "Cabaret",
+  "Cabaret",
+  "Classics Brunch",
+  "Classic Cinema: Safety First",
+  "Exit Stage Left",
+  "Antonio Perez"
+];
+
+let eventPrices = [
+  "$22/$36/$48",
+  "$48/$64/$88",
+  "$12",
+  "$18/$32",
+  "$24/$36",
+  "$48/$64/$88",
+  "$48/$64/$88",
+  "$12",
+  "$12",
+  "$18/$28/$36",
+  "$32/$48/$64"
+];
